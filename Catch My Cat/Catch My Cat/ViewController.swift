cd202669892338b9cbc22d9074e6e3ea143621e4
Yogesh Rathore <yogeshrathore251@gmail.com>
2023-12-21 18:50:12 +0530

    # YOGESH RATHORE:- UI design has completed.

--- Catch My Cat/Catch My Cat/ViewController.swift
+++ Catch My Cat/Catch My Cat/ViewController.swift
@@ -9,6 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var highScoreLabel: UILabel!
+    @IBOutlet weak var catImageView1: UIImageView!
+    @IBOutlet weak var catImageView2: UIImageView!
+    @IBOutlet weak var catImageView3: UIImageView!
+    @IBOutlet weak var catImageView4: UIImageView!
+    @IBOutlet weak var catImageView5: UIImageView!
+    @IBOutlet weak var catImageView6: UIImageView!
+    @IBOutlet weak var catImageView7: UIImageView!
+    @IBOutlet weak var catImageView8: UIImageView!
+    @IBOutlet weak var catImageView9: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
